e069e65002f134ffc6b7dd301a3001392fda394b
ibrahim almouhsin <ibrahimalmouhsin@gmail.com>
2022-06-05 11:28:10 +0300

    adding graphql moduel and global config moduel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLConfig } from './configurations/GraphQLConfig';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useClass: GraphQLConfig,
+    }),
+    forwardRef(() => PostsModule),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
